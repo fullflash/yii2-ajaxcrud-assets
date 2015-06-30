546d96c8f1a9349d77d8cced94c30bfacf850ebb
John Martin <john.itvn@gmail.com>
2015-06-30 16:02:46 +0700

    Update
    Update

---
+++ CrudAsset.php
@@ -0,0 +1,28 @@
+<?php
+namespace johnitvn\ajaxcrudassets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author John Martin <john.itvn@gmail.com>
+ * @since 1.0
+ */
+class CrudAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/johnitvn/yii2-ajaxcrud-assets/assets';
+    public $publishOptions = [
+        'forceCopy' => true,
+    ];
+    public $css = [
+    ];
+    public $js = [
+        //'ajaxcrud.js',
+        'ajaxcrud.min.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'kartik\grid\GridViewAsset',
+    ];
+
+}
